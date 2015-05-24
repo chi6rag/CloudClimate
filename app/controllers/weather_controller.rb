2fc2f67fefdd6d48f5ae144de26e1acd84146b8e
Chirag Aggarwal <chi6rag@gmail.com>
2015-05-24 21:38:58 +0530

    Add API implementation

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,8 +1,17 @@
 class WeatherController < ApplicationController
+	
 	def home
 	end
 
 	def results
-		fail
+		@location = params[:location]
+		@info = get_weather_info(params[:lat], params[:lng])
+	end
+
+	private
+	def get_weather_info(latitude, longitude)
+		weather = Weather.get('https://api.forecast.io/forecast/f6c94e488810a55e46971fa3fe534696/' \
+		                       + latitude.to_s + ',' + longitude.to_s + '?units=si')
+		weather.to_h
 	end
 end
---
+++ app/models/weather.rb
@@ -0,0 +1,3 @@
+class Weather
+	include HTTParty
+end
