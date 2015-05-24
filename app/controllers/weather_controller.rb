7ed3ba672067aaa519f6578cb4b1527696386d3f
Chirag Aggarwal <chi6rag@gmail.com>
2015-05-24 16:30:32 +0530

    Add geocomplete functionality; add weather controllers

---
+++ app/controllers/weather_controller.rb
@@ -0,0 +1,8 @@
+class WeatherController < ApplicationController
+	def home
+	end
+
+	def results
+		fail
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  root to: 'static_pages#home'
+  get 'weather/home'
+  post 'weather/results'
+  root to: 'weather#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
