dc87dc171610aaa804c9a4573743a33cea1992a4
Chirag Aggarwal <chi6rag@gmail.com>
2015-05-24 16:42:34 +0530

    Get the geocoding working

--- app/assets/javascripts/weather.js
+++ app/assets/javascripts/weather.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
-	$('.search_city').geocomplete();
+	$('.search_city_field').geocomplete({ details: ".search_city_form" });
+	$('.search_city_button').click(function(){
+		$('.search_city_field').trigger('geocode');
+	});
 });
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'weather/home'
-  post 'weather/results'
+  post 'results' => 'weather#results'
   root to: 'weather#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
